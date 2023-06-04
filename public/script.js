c99a8c7fc431f173fcfcbe561877b12f527406f4
Shibn2 <shibnstdy@gmail.com>
2023-06-04 11:31:46 +0530

    added html and script file

---
+++ public/script.js
@@ -0,0 +1,7 @@
+const rootEle = document.getElementById("root");
+
+const newEl = document.createElement('div');
+newEl.innerText = 'Shibin';
+newEl.style = ' color: red; width: 100px; height: 200px; background-color: green;'
+
+rootEle.appendChild(newEl)
